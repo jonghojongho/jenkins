52eb5ed35c139ecd0770ca5ca74df857df74c502
jonghojongho <whdgh9403@naver.com>
2019-12-03 13:32:18 +0900

    te
    te

---
+++ message_190850.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Our Mission' => 'Our Mission',
+    '아이디를 입력하세요' => 'E-mail',
+    '비밀번호를 입력하세요' => 'Password',
+    '비밀번호 찾기' => 'Forgot Password?',
+    '서비스 둘러보기' => 'Skip for now',
+
+    '캠페인' => 'Campaign',
+    '하이파이브 모먼트' => 'Highfive Moment',
+    '캠페인을 제작해보세요!' => 'Start your campaign!',
+    '개인정보처리방침' => 'Privacy Policy',
+    '이용약관' => 'Terms of Service',
+
+    '계정이 없으신가요?' => 'No account yet?',
+    '회원가입' => 'Sign Up',
+    '로그인' => 'Log In',
+    
+    '비밀번호 찾기' => 'Forgot Password',
+    '이메일' => 'e-mail',
+    '비밀번호 찾기' => 'Reset my Password',
+    '계정을 잃어버리셨다면?' => 'Forgotten account?',
+    '이메일 형식에 맞게 입력해주세요' => 'Wrong e-mail format',
+    '가입되지 않은 이메일입니다' => 'Invalid e-mail address',
+
+    '가입하신 이메일 주소' =>  'A temporary password has been sent to',
+    '로 임시 비밀번호를 발급하였습니다.' => '',
+    '임시 비밀번호를 통해 로그인하시고 꼭 비밀번호를 변경하시기 바랍니다.' => 'Please log in using temporary password. Make sure to change the password ',
+    '이용해주셔서 감사합니다' => 'Thank you',
+
+    '회원가입' => 'Sign Up',
+    '비밀번호' => 'Password',
+    '비밀번호 확인' => 'Confirm Passwod',
+    '닉네임' => 'Display name',
+    '성별' => 'Gender',
+    '국가' => 'Country',
+    '나이' => 'Year of Birth',
+    '셰어더밸류의 이용약관 및 개인정보처리방침에 동의합니다.' => 'I accept SharetheValue Privacy Policy and Terms of Serivce ',
+    '회원가입 완료' => 'Sign Up',
+
+    '관심이슈 연결' => 'Issue of Interest',
+    '관심있는 이슈를 한개이상 선택해보세요' => 'Choose 1 or more issues of interest',
+    '선택된 키워드를 지속적으로 보여드립니다' => 'chosen issues of interest will be updated to you continously',
+    '관심이슈 선택완료' => 'confirm issue of interest',
+
+    '이런 이슈도 알고계셨나요?' => 'Did you also know?',
+    '하이파이브' => 'Highfive!',
+    '추가완료' => 'Highfived!',
+    '셰어더밸류에서 캠페인을 지지하는 표현방식입니다. 공감하는 캠페인에 하이파이브를 해보세요.' => '`Highfive` means to support the campaigns in SharetheValue. Don`t hesitate to Highfive for the campaigns you support!',
+    '하이파이브란?' => 'What dose `Highfive` mean?',
+    'business' => '#201, 34-13, Jomaru-ro 105beon-gil, Bucheon-si, Gyeonggi-do, Republic of Korea',
+];
